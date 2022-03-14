c5f646beb29076a517ffb1f09d768fbdb1798580
Merve Akdeniz <merve.akdeniz1@ogr.sakarya.edu.tr>
2022-03-14 17:07:55 +0300

    14.03.2022

---
+++ .dist/js/footer.js
@@ -0,0 +1,102 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+
+    footer.innerHTML = `
+<div class="footer-content">
+    <img src="img/light-logo.png" class="logo" alt="">
+   <div class="footer-ul-container">
+        <ul class="category">
+            <li class="category-title">men</li>
+            <li>
+                <a href="#" class="footer-link">t-shirts</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">sweatshirts</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">shirts</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">jeans</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">trousers</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">shoes</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">casuals</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">formals</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">sports</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">watch</a>
+            </li>
+
+        </ul>
+        <ul class="category">
+            <li class="category-title">women</li>
+            <li>
+                <a href="#" class="footer-link">t-shirts</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">sweatshirts</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">shirts</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">jeans</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">trousers</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">shoes</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">casuals</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">formals</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">sports</a>
+            </li>
+            <li>
+                <a href="#" class="footer-link">watch</a>
+            </li>
+
+        </ul>
+    </div>
+
+</div>
+<p class="footer-title">about company</p>
+<p class="info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt, aspernatur facilis ullam commodi rem explicabo facere, reprehenderit cumque atque consequuntur, pariatur saepe tenetur obcaecati autem. Obcaecati ab a blanditiis repellendus
+    sequi reiciendis explicabo ut voluptatem doloribus rerum eaque, ex nulla nobis delectus totam tempora, magni adipisci consectetur accusamus! Consequuntur laborum, sapiente error quo temporibus earum voluptatibus nesciunt voluptas sed nostrum
+    excepturi necessitatibus possimus dolorem sint itaque labore a quaerat aliquam? Nihil, enim, sit numquam doloribus saepe dolorem consequuntur exercitationem ipsa minus, architecto iste adipisci quia vel cumque ullam voluptas laudantium rerum
+    assumenda perferendis qui alias voluptatum a itaque. Ut.</p>
+<p class="info">support emails - help@clothing.com, costumersupport@clothing.com</p>
+<p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+<div class="footer-social-container">
+    <div>
+        <a href="#" class="social-link">terms & services</a>
+        <a href="#" class="social-link">privacy page</a>
+    </div>
+    <div>
+        <a href="#" class="social-link">instagram</a>
+        <a href="#" class="social-link">facebook</a>
+        <a href="#" class="social-link">twitter</a>
+    </div>
+</div>
+<p class="footer-credit">Clothing, Best apparels online store</p>
+    
+    `;
+}
+createFooter();
